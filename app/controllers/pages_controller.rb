7c6c6cb74a140a38c82a3e60dc2ce4dfb00640a7
Jon Gray <jgray@spartaglobal.com>
2018-11-01 18:18:25 +0000

    Attempted structure for RESTful routes, also managed to correct database connection issue. Many errors remain.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,48 @@ class PagesController < ApplicationController
   end
 
   def dishes
-    @dish = Dish.find(params[:id])
+    @dish = Dish.find(params[:id].to_i)
   end
+
+  def new_chef
+
 
   def index
     @chefs = Chef.all
     @dishes = Dish.all
   end
 
-  def create
-    @chef = Chef.new(params[:id])
-    @chef.save
+  def createC
+    @chef = Chef.create name:params["name"] age:params["age"]
+    redirect '/'
   end
 
+  def updateC
+    @chef = Chef.find(params[:id].to_i)
+    @chef.update_attributes: name:params["name"] age:params["age"]
+    redirect '/'
+  end
 
+  def createD
+    @dish = Dish.create name:params["name"] rating:params["rating"] vege_or_no:params["vege_or_no"]
+    redirect '/'
+  end
+
+  def updateD
+    @dish = Dish.find(params[:id].to_i)
+    @dish.update_attributes: name:params["name"] rating:params["rating"] vege_or_no:params["vege_or_no"]
+    redirect '/'
+  end
+
+  def deleteD
+    @dish = Dish.find(params[:id].to_i)
+    @dish.destroy
+    redirect '/'
+  end
+
+  def deleteC
+    @chef = Chef.find(params[:id].to_i)
+    @chef.destroy
+    redirect '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  get '/', to: "pages#index"
+  root 'pages#index'
   get 'chefs/new', to: "pages#new_chef"
+  get 'chefs/:id/edit', to: "pages#edit_chef"
+  post 'chefs/new', to: "pages#createC"
+  put 'chefs/:id/edit', to: "pages#updateC"
+  delete 'chefs/:id/edit', to: "pages#deleteC"
+
+  get 'dishes/new', to: "pages#new_dish"
+  get 'dishes/:id/edit', to: "pages#edit_dish"
+  post 'dishes/new', to: "pages#createD"
+  put 'dishes/:id/edit', to: "pages#updateD"
+  delete 'chefs/:id/edit', to: "pages#deleteD"
+
   get 'chefs/:id', to: "pages#chefs"
   get 'dishes/:id', to: "pages#dishes"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
